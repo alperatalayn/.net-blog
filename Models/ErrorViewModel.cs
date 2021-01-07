3673eb590dd69f638e6a673177b47459baa6a9e5
alperatalayn <alperatalayn@gmail.com>
2021-01-07 16:59:00 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _net_blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
